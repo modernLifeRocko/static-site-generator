e80d9b19947239adde05397e2bb5ff0eb2f92208
Arch rocko <andres@fontalvo.xyz>
2024-11-02 16:46:50 -0500

    feat: add LeafNode

---
+++ src/htmlnode.py
@@ -0,0 +1,43 @@
+class HTMLNode():
+    def __init__(self, tag: str | None = None,
+                 value: str | None = None,
+                 children: list | None = None,
+                 props: dict[str, str] | None = None):
+        self.tag: str | None = tag
+        self.value: str | None = value
+        self.children: list | None = children
+        self.props: dict[str, str] | None = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        propstr = ""
+        if self.props is None:
+            return propstr
+        for key, val in self.props.items():
+            propstr += f" {key}: {val}"
+        return propstr
+
+    def __repr__(self):
+        return f'''
+        tag: {self.tag}
+        value: {self.value}
+        children: {self.children}
+        props: {self.props_to_html()}'''
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None,
+                 value: str,
+                 props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, dict)
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+
+        if self.tag == 'img':
+            return f'<{self.tag}{self.props_to_html()}/>'
+        else:
+            return f"<{self.tag}{self.props_to_html}>{self.value}</{self.tag}>"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class HTMLNodeTest(unittest.TestCase):
+    def testpropsnochild(self):
+        node = HTMLNode('p', 'this is a paragraph', None,
+                        {'color': 'black', 'margin': 'auto'})
+        print(node)
+        propsstr = node.props_to_html()
+        self.assertEqual(propsstr, ' color: black margin: auto')
+
+    def testnopropschild(self):
+        node1 = HTMLNode('li', 'first list item')
+        node2 = HTMLNode('li', 'second list item')
+
+        listnode = HTMLNode('ol', None, [node1, node2])
+        print(listnode)
+
+        propstr = listnode.props_to_html()
+        self.assertEqual(propstr, '')
+
+    def testchildprop(self):
+        node1 = HTMLNode('p', 'this is text')
+        pnode = HTMLNode('div', None, [node1], {'margin': 'auto'})
+
+        pstr = pnode.props_to_html()
+        self.assertEqual(pstr, ' margin: auto')
